02db00f9c342a0d7f1babcb42f0f77fb0a00e401
ROMASHSX <andriiromash@gmail.com>
2021-04-04 13:45:23 +0300

    entities are created

---
+++ src/main/java/com/ntu/romash/StudentEntity.java
@@ -0,0 +1,49 @@
+package com.ntu.romash;
+
+public class StudentEntity {
+    private String firstName;
+    private String lastName;
+    private FacultyEntity faculty;
+    private int course;
+    private int averageGrade;
+
+    public int getAverageGrade() {
+        return averageGrade;
+    }
+
+    public void setAverageGrade(int averageGrade) {
+        this.averageGrade = averageGrade;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public FacultyEntity getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(FacultyEntity faculty) {
+        this.faculty = faculty;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+}
